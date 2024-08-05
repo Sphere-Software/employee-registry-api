16f9788002f17de0b510f66d47973c98fce1827b
Toplica Tanasković <toplicius@gmail.com>
2024-08-05 15:21:34 +0200

    chore: Added Flask-RESTX support.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
