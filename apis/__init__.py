070d8a25e3e6d1201f94f8a6df71ea8ec196bd63
Toplica Tanasković <toplicius@gmail.com>
2024-08-07 13:45:56 +0200

    feat(api): Added version information endpoint.

--- apis/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restx import Api
-
-from .version import api as ns1
-
-api = Api(
-    title="Employee registry", version="0.0.1", description="Employee registry API"
-)
-
-api.add_namespace(ns1, "/version")
