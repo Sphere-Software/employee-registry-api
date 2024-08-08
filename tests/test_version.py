1fc5dce87f5c954fc23bca1c7c1c6dbe2d3325e0
Toplica Tanasković <toplicius@gmail.com>
2024-08-08 08:50:42 +0200

    fix(test): Version test now reads project version from pyproject.toml

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,7 +1,10 @@
 import json
+import toml
 
 
 def test_version(client):
+    with open("pyproject.toml", "r") as f:
+        expected = toml.load(f)
     response = client.get("/version", follow_redirects=True)
     version_info = json.loads(response.data)
-    assert version_info["version"] == "0.1.0"
+    assert version_info["version"] == expected["project"]["version"]
